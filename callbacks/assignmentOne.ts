fc3379167c26948c54a6a8979774968f5397f2b8
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-03 11:56:27 +0200

    Add assignment one

---
+++ callbacks/assignmentOne.ts
@@ -0,0 +1,11 @@
+type helloFunction = (message: string) => void;
+
+const greeting = (hello: helloFunction): void => {
+  hello("Hello from callback!");
+};
+
+const hello = (message: string): void => {
+  console.log(message);
+};
+
+greeting(hello);
