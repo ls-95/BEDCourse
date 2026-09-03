ea489545ae6190c4edc3b5d78bb3648d69afec87
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-03 14:31:18 +0200

    Fix spelling mistake. Change to upper case method.

--- callbacks/assignmentFour.ts
+++ callbacks/assignmentFour.ts
@@ -16,13 +16,17 @@ type displaySentenceFunction = (sentence: string) => void;
 
 const transformToUpperCase = (
   sentence: string,
-  dislpaySentence: displaySentenceFunction,
+  displaySentence: displaySentenceFunction,
 ): void => {
-  dislpaySentence(sentence.toLocaleUpperCase());
+  displaySentence(sentence.toUpperCase()); //Global Unicode mapping
+  // dislpaySentence(sentence.toLocaleUpperCase("fr-FR")); -> checks locale rule first if should override global mapping - fr-FR for french
 };
 
-const dislpaySentence = (sentence: string): void => {
+const displaySentence = (sentence: string): void => {
   console.log(sentence);
 };
 
-transformToUpperCase("hello world", dislpaySentence);
+transformToUpperCase(
+  "Bonjour, comment ça va? Je vais bien merci, je vais au marché aujourd'hui",
+  displaySentence,
+);
