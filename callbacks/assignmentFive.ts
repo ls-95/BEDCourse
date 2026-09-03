cf0a680c4f443f26b2769016ad224ecad5a7392a
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-03 14:47:12 +0200

    Refactored assignment five

--- callbacks/assignmentFive.ts
+++ callbacks/assignmentFive.ts
@@ -3,9 +3,17 @@
 type orderStatusFunction = (status: string) => void;
 
 const orderPizza = (orderStatus: orderStatusFunction): void => {
+  console.log(
+    "You have ordered a pizza! Your pizza will be ready in record time!",
+  );
   setTimeout(() => {
-    orderStatus("Your pizza is ready!");
+    orderStatus("Your pizza is ready! Told you!");
   }, 3000);
+  setTimeout(() => {
+    orderStatus(
+      "Please come and get your pizza.. it also gets cold in record time..",
+    );
+  }, 6000);
 };
 
 const orderStatus = (status: string): void => {
