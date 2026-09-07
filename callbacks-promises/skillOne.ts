9698a6c23c80960b378251f0a95ad005630b95c5
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-07 16:39:15 +0200

    Add skill one content

---
+++ callbacks-promises/skillOne.ts
@@ -0,0 +1,55 @@
+//Order Confirmation
+
+type OrderCallback = () => void;
+
+const placeOrder = (item: string, callback: OrderCallback) => {
+  console.log(`Order placed for ${item}`);
+  callback();
+};
+
+const callbackFunction = () => {
+  console.log("Thanks for your order!");
+};
+
+placeOrder("Pen", callbackFunction);
+
+//Sum with a Named Type
+
+type SumCallback = (result: number) => void;
+
+const sumNumbers = (a: number, b: number, callback: SumCallback) => {
+  callback(a + b);
+};
+
+const addition = (result: number): void => {
+  console.log(result);
+};
+
+sumNumbers(3, 5, addition);
+
+//Challenge (optional): Callback with Multiple Parameters
+
+type CompareCallback = (a: number, b: number) => string;
+
+const compareNumbers = (
+  a: number,
+  b: number,
+  callback: CompareCallback,
+): string => {
+  return callback(a, b);
+};
+
+console.log(
+  compareNumbers(3, 5, (a: number, b: number) => {
+    return a > b ? "a is bigger" : a === b ? "equal" : "b is bigger";
+  }),
+);
+console.log(
+  compareNumbers(87, 34, (a: number, b: number) => {
+    return a > b
+      ? `The difference is ${a - b}!`
+      : a === b
+        ? "There is no difference!"
+        : `The difference is ${b - a}`;
+  }),
+);
