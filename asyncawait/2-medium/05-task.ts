5f27cca0f416a84a18f5700e100fb687d75e5927
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-09 14:50:03 +0200

    Add task five content

---
+++ asyncawait/2-medium/05-task.ts
@@ -0,0 +1,33 @@
+/*
+
+const fetchAdviceById = (id: number) => {
+  fetch(`https://api.adviceslip.com/advice/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Fetching did not work");
+      }
+      return response.json();
+    })
+    .then((data) => console.log(`Advice ID: ${id}: ${data.slip.advice}`))
+    .catch((error) => console.log("Error fetching advice:", error));
+};
+
+*/
+
+const fetchAdvice = async (id: number): Promise<void> => {
+  try {
+    const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
+    if (!response.ok) {
+      throw new Error("Fetching did not work");
+    }
+    const data = await response.json();
+    if (!data.slip) {
+      throw new Error(`Couldn't find advice with id "${id}"`);
+    }
+    console.log(`Advice ID: ${id}: ${data.slip.advice}`);
+  } catch (error) {
+    console.log("Error fetching advice:", error);
+  }
+};
+
+fetchAdvice(9999999);
