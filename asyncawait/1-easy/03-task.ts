cbc1f43cb96e72f326cb0beb20dbb9b080a21864
ls-95 <143622355+ls-95@users.noreply.github.com>
2026-09-09 13:55:40 +0200

    Fix: file name spelling mistake. Feat: add task three content

---
+++ asyncawait/1-easy/03-task.ts
@@ -0,0 +1,40 @@
+//Delayed Message
+
+/* .then() .catch()
+
+const delayedMessage = (message: string, delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+};
+
+delayedMessage("This is a delayed message", 2000)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log("Error: ", error);
+  });
+
+  */
+
+const delayedMessage = (message: string, delay: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(message);
+    }, delay);
+  });
+};
+
+const result = async () => {
+  try {
+    const message = await delayedMessage("This is a delayed message", 3000);
+    console.log(message);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+result();
